c0e85db46c616f06dfe5d14f37e74febe6c0954e
Hong jimin <hsk3232@pusan.ac.kr>
2025-10-20 00:04:57 +0900

    10/20

--- src/main/java/edu/pnu/domain/AssetRoute.java
+++ src/main/java/edu/pnu/domain/AssetRoute.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,6 +22,7 @@ import lombok.ToString;
 @ToString
 @Entity
 @Builder
+@Table(name ="assetroute")
 public class AssetRoute {
 	 @Id 
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
